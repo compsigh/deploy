04c82c2e0520714ee487699f21c55e9b9a2edf04
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-10-16 21:53:20 -0700

    Left-align title

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home () {
   return (
     <main className={styles.main}>
-      <h1><span className='playbutton'>► </span>DEPLOY/23</h1>
+      <h1 className={styles.title}><span className={styles.playbutton}>► </span>DEPLOY/23</h1>
     </main>
   )
 }
