e2cad3136f80ab1331cec23b011baa5d65401cdc
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-10-16 21:45:56 -0700

    Add fade to play button

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home () {
   return (
     <main className={styles.main}>
-      <h1>► DEPLOY/23</h1>
+      <h1><span className='playbutton'>► </span>DEPLOY/23</h1>
     </main>
   )
 }
