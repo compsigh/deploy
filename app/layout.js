ccbaf633f8f34842f919a32c8f26c89814aabc8c
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-10-20 09:37:13 -0700

    Serve fonts at build time

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,59 @@
 import AuthWrapper from '@/components/AuthWrapper'
 
+import localFont from 'next/font/local'
 import './globals.css'
+
+const ProtoMono = localFont({
+  src: [
+    {
+      path: '../public/fonts/ProtoMono-Light.woff',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: '../public/fonts/ProtoMono-Regular.woff',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../public/fonts/ProtoMono-Medium.woff',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: '../public/fonts/ProtoMono-SemiBold.woff',
+      weight: '600',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-proto-mono'
+})
+
+const iAWriterQuattro = localFont({
+  src: [
+    {
+      path: '../public/fonts/iAWriterQuattroS-Regular.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../public/fonts/iAWriterQuattroS-Italic.woff2',
+      weight: '400',
+      style: 'italic'
+    },
+    {
+      path: '../public/fonts/iAWriterQuattroS-Bold.woff2',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: '../public/fonts/iAWriterQuattroS-BoldItalic.woff2',
+      weight: '600',
+      style: 'italic'
+    }
+  ],
+  variable: '--font-ia-writer-quattro'
+})
 
 export const metadata = {
   title: 'DEPLOY/23',
@@ -10,7 +63,7 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <AuthWrapper>
-      <html lang="en">
+      <html lang="en" className={`${ProtoMono.variable} ${iAWriterQuattro.variable}`}>
         <body>{children}</body>
       </html>
     </AuthWrapper>
