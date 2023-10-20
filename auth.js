5d715d755806dcb92b63a7a6f90d948fd9aded07
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-10-19 21:12:04 -0700

    Configure NextAuth

---
+++ auth.js
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile (profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture
+        }
+      }
+    })
+  ]
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
