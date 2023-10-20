a1891339e8cba8f145ffa3ee533675bd948df684
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-10-20 00:36:06 -0700

    Embed participant registration form

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,6 @@
 // Next imports
 import { redirect } from 'next/navigation'
+import Script from 'next/script'
 
 // Auth imports
 import { getSessionData } from '@/functions/user-management'
@@ -14,6 +15,18 @@ export default async function Dashboard () {
       <h1>Authenticated as:</h1>
       <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
+
+      <Script async src="https://tally.so/widgets/embed.js" />
+      <iframe
+        data-tally-src="https://tally.so/embed/w7KEWZ?transparentBackground=1&dynamicHeight=1"
+        loading="lazy"
+        width="100%"
+        height="146"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        title="DEPLOY/23 Participant Registration">
+      </iframe>
     </>
   )
 }
