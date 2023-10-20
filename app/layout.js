b9702c846d80603b9becc4513064522a57ab8457
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-10-19 22:03:53 -0700

    Make fonts externally accessible

--- app/layout.js
+++ app/layout.js
@@ -1,62 +1,6 @@
 import AuthWrapper from '@/components/AuthWrapper'
 
-import localFont from 'next/font/local'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
-
-const ProtoMono = localFont({
-  src: [
-    {
-      path: './fonts/ProtoMono-Light.woff',
-      weight: '300',
-      style: 'normal'
-    },
-    {
-      path: './fonts/ProtoMono-Regular.woff',
-      weight: '400',
-      style: 'normal'
-    },
-    {
-      path: './fonts/ProtoMono-Medium.woff',
-      weight: '500',
-      style: 'normal'
-    },
-    {
-      path: './fonts/ProtoMono-SemiBold.woff',
-      weight: '600',
-      style: 'normal'
-    }
-  ],
-  variable: '--font-proto-mono'
-})
-
-const iAWriterQuattro = localFont({
-  src: [
-    {
-      path: './fonts/iAWriterQuattroS-Regular.woff2',
-      weight: '400',
-      style: 'normal'
-    },
-    {
-      path: './fonts/iAWriterQuattroS-Bold.woff2',
-      weight: '700',
-      style: 'normal'
-    },
-    {
-      path: './fonts/iAWriterQuattroS-Italic.woff2',
-      weight: '400',
-      style: 'italic'
-    },
-    {
-      path: './fonts/iAWriterQuattroS-BoldItalic.woff2',
-      weight: '700',
-      style: 'italic'
-    }
-  ],
-  variable: '--font-ia-writer-quattro'
-})
 
 export const metadata = {
   title: 'DEPLOY/23',
@@ -66,7 +10,7 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <AuthWrapper>
-      <html lang="en" className={`${inter.variable} ${ProtoMono.variable} ${iAWriterQuattro.variable}`}>
+      <html lang="en">
         <body>{children}</body>
       </html>
     </AuthWrapper>
