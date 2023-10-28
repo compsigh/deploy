e10f5e4e552161e62de5620dfc73d25da1dcae6f
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-10-27 20:40:17 -0700

    Bump version to `v1.2.1`

--- app/page.js
+++ app/page.js
@@ -268,7 +268,7 @@ export default async function Home () {
             height={10}
             className='fade'
           />
-          <p>v1.0.0 | <Link href={'https://github.com/compsigh/deploy'} target='_blank'>Source Code</Link></p>
+          <p>v1.2.1 | <Link href={'https://github.com/compsigh/deploy'} target='_blank'>Source Code</Link></p>
         </div>
 
         <div className={styles['logo-wrapper']}>
