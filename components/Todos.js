e0c918b756c5a2830a6f1a51bd78ae1597b23bf7
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-11-17 18:57:40 -0800

    Open team declaration form in advance

--- components/Todos.js
+++ components/Todos.js
@@ -7,7 +7,8 @@ import styles from '@/app/console/Console.module.css'
 export default async function Todos ({ user }) {
   const teamDeclarationOpenDatetime = new Date('2023-11-17T20:00:00-08:00')
   const projectSubmissionOpenDatetime = new Date('2023-11-19T09:00:00-08:00')
-  const teamDeclarationOpen = isOpen(teamDeclarationOpenDatetime)
+  let teamDeclarationOpen = isOpen(teamDeclarationOpenDatetime)
+  teamDeclarationOpen = true
   const projectSubmissionOpen = isOpen(projectSubmissionOpenDatetime)
   const peoplesChoiceVoteOpen = false
 
