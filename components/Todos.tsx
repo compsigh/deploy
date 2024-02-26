f8eb3db7b24e24dee62ad4670f308c0547cc8a89
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-02-25 21:47:07 -0800

    Add data fetching to Todos component

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -1,20 +1,28 @@
-// Next imports
+// Next
 import { get } from '@vercel/edge-config'
 
-// Component imports
-import Button from '@/components/Button'
+// Auth
+import { isStudent } from '@/functions/user-management'
+import type { User } from 'next-auth'
 
-// Style imports
+// Components
+import { Button } from '@/components/Button'
+
+// Functions
+import { fetchParticipantNotionPage, fetchParticipantTeamNotionPage, peoplesChoiceVoteFound, submissionFound } from '@/functions/notion'
+import type { TitlePagePropertyType } from '@/functions/notion'
+
+// Styles
 import styles from '@/app/console/Console.module.css'
 
-export async function Todos ({ user }) {
+export async function Todos ({ user }: { user: User }) {
   const teamDeclarationOpenDatetime = new Date('2023-11-17T20:00:00-08:00')
   const projectSubmissionOpenDatetime = new Date('2023-11-19T09:00:00-08:00')
   const teamDeclarationOpen = get('teamDeclarationOpen') || isOpen(teamDeclarationOpenDatetime)
   const projectSubmissionOpen = get('projectSubmissionOpen') || isOpen(projectSubmissionOpenDatetime)
   const peoplesChoiceVoteOpen = get('peoplesChoiceVoteOpen') || false
 
-  if (user.judge)
+  if (!isStudent(user.email))
     return (
       <ul className={styles.todos}>
         <li className='fade'><Button text='Evaluate a project' type='link' destination='/evaluate' user={user} /></li>
@@ -23,10 +31,17 @@ export async function Todos ({ user }) {
       </ul>
     )
 
+  const participant = await fetchParticipantNotionPage(user)
+  const team = await fetchParticipantTeamNotionPage(participant)
+  const teamNameProperty = team?.properties.Name as TitlePagePropertyType
+  const teamName = teamNameProperty.title[0].text.content
+  const hasSubmitted = await submissionFound(participant)
+  const hasVoted = await peoplesChoiceVoteFound(participant)
+
   return (
     <ul className={styles.todos}>
       {
-        user.participant
+        participant
           ? <li style={{ color: '#888888' }}>You&apos;ve registered        <p className='comment'>you&apos;re all set to attend</p></li>
           : <li className='fade'><Button text='Register' type='link' destination='/register' user={user} /></li>
       }
@@ -36,11 +51,11 @@ export async function Todos ({ user }) {
             <li>Declare your team        <p className='comment'>open Friday, Nov. 17 8pm</p></li>
       }
       {
-        teamDeclarationOpen && user.participant && !user.participant?.teamName &&
+        teamDeclarationOpen && participant && !team &&
             <li className='fade'><Button text='Declare your team' type='link' destination={`/team?participant_email=${user.email}`} /></li>
       }
       {
-        teamDeclarationOpen && user.participant?.teamName &&
+        teamDeclarationOpen && participant && team &&
             <li style={{ color: '#888888' }}>You&apos;re on a team         <p className='comment'>you&apos;re all set to submit</p></li>
       }
 
@@ -49,11 +64,11 @@ export async function Todos ({ user }) {
             <li>Submit your project      <p className='comment'>open Sunday, Nov. 19 9am</p></li>
       }
       {
-        projectSubmissionOpen && user.participant && user.participant?.teamName && !user.participant?.submitted &&
-            <li className='fade'><Button text='Submit your project' type='link' destination={`/submit?participant_email=${user.email}&teamname=${user.participant?.teamName}`} /></li>
+        projectSubmissionOpen && participant && team && !hasSubmitted &&
+            <li className='fade'><Button text='Submit your project' type='link' destination={`/submit?participant_email=${user.email}&teamname=${teamName}`} /></li>
       }
       {
-        projectSubmissionOpen && user.participant?.submitted &&
+        projectSubmissionOpen && hasSubmitted &&
             <li style={{ color: '#888888' }}>You&apos;ve submitted         <p className='comment'>you&apos;re ready, good luck!</p></li>
       }
 
@@ -62,11 +77,11 @@ export async function Todos ({ user }) {
             <li>Vote for People&apos;s Choice <p className='comment'>open after presentations</p></li>
       }
       {
-        peoplesChoiceVoteOpen && user.participant && user.participant?.teamName && !user.participant?.voted &&
-            <li className='fade'><Button text='Vote for People&apos;s Choice' type='link' destination={`/vote?participant_email=${user.email}&teamname=${user.participant?.teamName}`} /></li>
+        peoplesChoiceVoteOpen && participant && team && !hasVoted &&
+            <li className='fade'><Button text='Vote for People&apos;s Choice' type='link' destination={`/vote?participant_email=${user.email}&teamname=${teamName}`} /></li>
       }
       {
-        peoplesChoiceVoteOpen && user.participant?.voted &&
+        peoplesChoiceVoteOpen && hasVoted &&
             <li style={{ color: '#888888' }}>You&apos;ve voted             <p className='comment'>thanks for participating</p></li>
       }
       <br />
@@ -77,7 +92,7 @@ export async function Todos ({ user }) {
   )
 }
 
-function isOpen (datetime) {
+function isOpen (datetime: Date) {
   const currentDate = new Date()
   if (currentDate >= datetime)
     return true
