543aceccc4d07e37504e7b3d573ba7e643ae3e8d
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-10-21 12:45:38 -0700

    Guarantee `User` object from `checkAuth()`

--- platform/functions/user-management.ts
+++ platform/functions/user-management.ts
@@ -1,12 +1,29 @@
 import { type Session } from 'next-auth'
 
-export function checkAuth(session: Session | null) {
-  if (!session?.user?.email?.endsWith('usfca.edu'))
-    return false
-  return session.user
+export type User = {
+  name: string
+  email: string
 }
 
-export function isStudent(email: string) {
-  if (email.endsWith('dons.usfca.edu'))
-    return true
+/**
+ * Checks if the current session's user is an authenticated USF account.
+ * Returns a guaranteed `User` object if so, `false` otherwise.
+ *
+ * @export
+ * @param {Session} session The current NextAuth session.
+ */
+export function checkAuth(session: Session | null): User | false {
+  if (!session)
+    return false
+  const { user } = session
+  if (!user)
+    return false
+  if (!user.email || !user.name)
+    return false
+  if (!user.email.endsWith('usfca.edu'))
+    return false
+  return {
+    name: user.name,
+    email: user.email
+  }
 }
